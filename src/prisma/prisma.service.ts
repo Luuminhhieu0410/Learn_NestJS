1b1121e7eb347552a849d6db12a3bc110960cdab
unknown <luu7940@gmail.com>
2025-07-27 23:18:38 +0700

    Create Prisma Module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  // lifecycle hook ( chạy ngay sau khi khởi tạo module và inject tất cả các dependency của class đó xong)
+  onModuleInit() {
+    this.$connect();
+  }
+  // Gọi khi module bị destroy (ví dụ app shutdown thì đóng kết nối DB).
+  onModuleDestroy() {
+    this.$disconnect();
+  }
+}
